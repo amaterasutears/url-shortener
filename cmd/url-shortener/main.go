5d32eed7a1b7a82a48f02d3f31121aee24e1f689
amaterasu tears <kirillufoo@gmail.com>
2024-07-05 20:53:35 +0300

    feat(deploy) added container with mongo

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,12 +1,36 @@
 package main
 
 import (
+	"context"
 	"log"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://mongo:27017"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		err := client.Disconnect(ctx)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	app := fiber.New()
 
 	app.Get("/health", func(c *fiber.Ctx) error {
@@ -15,7 +39,7 @@ func main() {
 		})
 	})
 
-	err := app.Listen(":8080")
+	err = app.Listen(":8080")
 	if err != nil {
 		log.Fatal(err)
 	}
